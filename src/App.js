fa94d7e931e6e2c845431f8df56385b856079b7b
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-07-15 15:13:23 -0500

    Replaced hard coded scores

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import BottomRow from "./BottomRow";
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  
   //You'll need one for the home score and another for the away score.
-  const [LionsScore, setLionsScore] = useState();
-  const [TigersScore, setTigersScore] = useState();
+  const [lionsScore, setLionsScore] = useState(0);
+  const [tigersScore, setTigersScore] = useState(0);
   return (
     <div className="container">
       <section className="scoreboard">
@@ -19,12 +19,12 @@ function App() {
             values in these divs to accept dynamic values from 
             our state. */}
 
-            <div className="home__score">32</div>
+            <div className="home__score">{lionsScore}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{tigersScore}</div>
           </div>
         </div>
         <BottomRow />
